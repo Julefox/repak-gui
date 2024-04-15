062264d95ba5a91b186d09f724d85352ef3dacd5
Julefox <Julien.aub13@gmail.com>
2024-04-15 18:17:35 +0200

    add RPakData & AssetData

---
+++ src/public/assetdata.h
@@ -0,0 +1,53 @@
+﻿#pragma once
+
+class AssetData
+{
+public:
+	explicit AssetData( const QString& type = {} ) : Type( type ) {}
+	virtual ~AssetData() = default;
+
+	QString Type;
+	QString Path;
+};
+
+class TextureData final : public AssetData
+{
+public:
+	TextureData() : AssetData( "txtr" ) {}
+};
+
+class ModelData final : public AssetData
+{
+public:
+	ModelData() : AssetData( "rmdl" ) {}
+};
+
+class UiImageData final : public AssetData
+{
+public:
+	UiImageData() : AssetData( "uimg" ) {}
+};
+
+class PatchData final : public AssetData
+{
+public:
+	PatchData() : AssetData( "Ptch" ) {}
+};
+
+class DataTableData final : public AssetData
+{
+public:
+	DataTableData() : AssetData( "dtbl" ) {}
+};
+
+class MaterialData final : public AssetData
+{
+public:
+	MaterialData() : AssetData( "matl" ) {}
+};
+
+class AnimationData final : public AssetData
+{
+public:
+	AnimationData() : AssetData( "rseq" ) {}
+};
---
+++ src/public/rpakdata.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "assetdata.h"
+
+class RPakData
+{
+public:
+	RPakData()  = default;
+	~RPakData() = default;
+
+	QString RPakName;
+	int RPakVersion         = 8; // 7 = Titanfall 2, 8 = Apex Legends
+	int MapVersion          = 1;
+	bool KeepDevOnly        = false;
+	QString AssetDirectory  = ".\\assets\\";
+	QString OutputDirectory = ".\\output\\";
+	QString StarPakPath;
+
+	// All AssetData
+	QList < TextureData > Textures;
+	QList < ModelData > Models;
+	QList < UiImageData > UiImages;
+	QList < PatchData > Patches;
+	QList < DataTableData > DataTables;
+	QList < MaterialData > Materials;
+	QList < AnimationData > Animations;
+};
